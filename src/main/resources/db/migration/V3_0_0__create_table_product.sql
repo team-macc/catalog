224d43c855ac04718069e8afa961e3c354c2ac65
Samuel Souza <samuelsouza.contact@gmail.com>
2020-12-08 09:40:11 -0300

    creating db migrations

---
+++ src/main/resources/db/migration/V3_0_0__create_table_product.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `product` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `category_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1mtsbur82frn64de7balymq9s` (`category_id`),
+  CONSTRAINT `FK1mtsbur82frn64de7balymq9s` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
